b961dee8b67659fd85a7d9d08902db7565fc0062
Muhammad-Harris55 <Harrisimran55@gmail.com>
2025-11-24 13:39:20 +0500

    Cors reversed

--- server.js
+++ server.js
@@ -14,10 +14,11 @@ const router = require("./Router/Routes");
 const cloudinary = require("cloudinary").v2;
 const cookieParser = require("cookie-parser");
 // Set up middlewares
-app.use(cors({
-  origin: ["http://localhost:5173", "https://arabiann.netlify.app/"], 
-  credentials: true      
-}));
+// app.use(cors({
+//   origin: ["http://localhost:5173", "https://arabiann.netlify.app/"], 
+//   credentials: true      
+// }));
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
