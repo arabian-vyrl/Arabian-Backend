d32b964116b90aaf3209c5a11d989b2f7529ceff
Adeel-116 <adeel8128377@gmail.com>
2026-02-21 05:48:26 +0500

    Adeel feat ( similar properties new offplan remove current, and fetch sugession form tags in news, blog, and podcast)

--- server.js
+++ server.js
@@ -91,9 +91,9 @@ const upload = multer({
 });
 
 // Agents with salesforce sync cron job (CRON JOBS)
-setupCronJobs();
-schedulePropertySync();
-scheduleNewOffPlanSync();
+// setupCronJobs();
+// schedulePropertySync();
+// scheduleNewOffPlanSync();
 
 // Then mount your API routes
 app.use("/", router);
