23ee5823a24b7336a9f5f85614f6bfdaa3ce917e
Muhammad-Harris55 <Harrisimran55@gmail.com>
2026-03-03 17:04:15 +0500

    Harris feat added Arabian domain

--- server.js
+++ server.js
@@ -158,6 +158,7 @@ app.use(
       "http://localhost:5174",
       "http://localhost:5175",
       "https://arabiann.netlify.app",
+      "https://arabianestates.ae"
     ],
     credentials: true,
   }),
